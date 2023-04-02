77e9e7c4a1761a7e6c4750f4d70607a6f0b302c3
Aboli Mahimkar <127722656+Abolimahimkar@users.noreply.github.com>
2023-04-02 23:52:24 +0530

    Add files via upload

---
+++ Arearect.java
@@ -0,0 +1,11 @@
+package Lab;
+
+public class Arearect {
+	      public static void main(String args[])  
+	    {  
+	    int lenght=5;  
+	    int breadth=10;  
+	    int area=lenght*breadth;  
+	        System.out.println("Area of rectangle="+area);  
+	     }  
+	}
